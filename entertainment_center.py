904a9c623a3e3036803b4bc8042159f0f7611004
Srinivasan <srini2351994@gmail.com>
2016-04-06 04:05:15 +0530

    Comments Added

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,9 +3,19 @@ import fresh_tomatoes
 
 
 class EntertainmentCenter():
+	"""
+	EntertainmentCenter Class instance generates movie database from file 
+	and uses open function to open the datasets on a webpage
+
+	Usage :
+
+	ent_center = EntertainmentCenter(<filename>)
+
+	"""
 	def __init__(self,filename):
 		self.filename=filename
 		self.movies=[]
+		#Loads movies text as Movie objects from file
 		f=open(self.filename,"r")
 		for line in f:
 			mov = line.split("\t")
@@ -14,10 +24,14 @@ class EntertainmentCenter():
 		self.open()
 
 	def open(self):
+		"""
+		Function to open the movie database on webpage using fresh_tomatoes template
+		"""
 		fresh_tomatoes.open_movies_page(self.movies)
 
 
 if __name__=="__main__":
+	# Uses movie dataset filename as input to generate an HTML file and open it in the browser.
 	EntertainmentCenter("movies.tsv")
 
 
--- media.py
+++ media.py
@@ -1,8 +1,17 @@
 class Movie():
+	"""
+	Movie Class instance stores the Title, Storyline, Thumbnail 
+	and Youtube URL of the Movie 
+
+	Usage :
+
+	movie = Movie(<title>,<storyline>,<thumbnail>,<yt_url>)
+
+	"""
 	def __init__(self,title,storyline,thumbnail,yt_url):
 		self.title = title
 		self.storyline = storyline
 		self.thumbnail = thumbnail
 		self.yt_url = yt_url
-		
+
 
